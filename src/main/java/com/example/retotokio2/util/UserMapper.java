b501accc517518d7b86e56dbf8e39d97e553f044
Edward Medrano <edwardgino13@gmail.com>
2025-08-08 15:49:47 -0400

    add UserMapper

---
+++ src/main/java/com/example/retotokio2/util/UserMapper.java
@@ -0,0 +1,24 @@
+package com.example.retotokio2.util;
+
+import com.example.retotokio2.dto.UserDto;
+import com.example.retotokio2.entity.UserEntity;
+
+public class UserMapper {
+    public static UserDto entityToDto(UserEntity user){
+        final UserDto dto = new UserDto();
+        dto.setIdentificador(user.getId());
+        dto.setNombre(user.getName());
+        dto.setEdad(user.getAge());
+        dto.setCorreo(user.getEmail());
+        return dto;
+    }
+
+    public static UserEntity dtoToEntity(UserDto dto){
+        final UserEntity user = new UserEntity();
+        user.setId(dto.getIdentificador());
+        user.setName(dto.getNombre());
+        user.setAge(dto.getEdad());
+        user.setEmail(dto.getCorreo());
+        return user;
+    }
+}

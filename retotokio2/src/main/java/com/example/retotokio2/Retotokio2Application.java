a292698bcd3b6a94a2cce6ceb0f01c9f304eefbc
Edward Medrano <edwardgino13@gmail.com>
2025-08-07 22:12:57 -0400

    agregando projecto Spring Boot

---
+++ retotokio2/src/main/java/com/example/retotokio2/Retotokio2Application.java
@@ -0,0 +1,13 @@
+package com.example.retotokio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Retotokio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Retotokio2Application.class, args);
+	}
+
+}

cdeaba01007a0c70add140b51412a8a9659bf53a
Edward Medrano <edwardgino13@gmail.com>
2025-08-07 22:36:21 -0400

    add UserDto

---
+++ src/main/java/com/example/retotokio2/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.retotokio2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String identificador;
+    private String nombre;
+    private int edad;
+    private String correo;
+}

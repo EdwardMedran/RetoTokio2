7ccdcbe04c54a36e78dd6c8d12e14c1b87aa948e
Edward Medrano <edwardgino13@gmail.com>
2025-08-08 20:24:36 -0400

    add UserController and UserService

---
+++ src/main/java/com/example/retotokio2/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.retotokio2.controller;
+
+import com.example.retotokio2.dto.UserDto;
+import com.example.retotokio2.service.impl.UserService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<UserDto>> getUser(@PathVariable String id){
+        return this.userService.getUser(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public Flux<ResponseEntity<UserDto>> getAllUser() {
+        return this.userService.getAllUser()
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<UserDto>> saveUser(@RequestBody UserDto dto){
+        return  this.userService.saveUser(dto)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+
+    }
+}
+
+
